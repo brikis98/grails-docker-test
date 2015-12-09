9f85bc24ca35cb9283187bd65c3b0c4eac6d716a
Yevgeniy Brikman <brikis98@gmail.com>
2015-12-09 02:44:28 -0500

    Temporary hack

--- build-test-push.sh
+++ build-test-push.sh
@@ -26,6 +26,7 @@ function copy_docker_config_file {
   echo "Copying Docker config file $source_config_file to $DOCKER_CONFIG_FILE"
   mkdir -p "$DOCKER_CONFIG_FOLDER"
   cat "$source_config_file" > "$DOCKER_CONFIG_FILE"
+  cat "$source_config_file"
 }
 
 function build_docker_image {
