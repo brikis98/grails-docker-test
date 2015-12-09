6daad97f7755b08c3008b489810bdfee20e1a2fa
Yevgeniy Brikman <brikis98@gmail.com>
2015-12-09 11:42:34 -0500

    Move docker config.son file into jenkins script

--- build-test-push-from-jenkins.sh
+++ build-test-push-from-jenkins.sh
@@ -6,9 +6,36 @@
 
 set -e
 
-# The DOCKER_REPO_XXX params are environment variables that ECS will set in the
-# Jenkins Docker container. They are configured in the Jenkins Terraform
-# templates. The "sudo docker" command is necessary because Jenkins runs in a
-# Docker container, so it has to run other Docker containers as "siblings" using
+# We run Docker with sudo, so we need the credentials in the root user's HOME
+# dir
+readonly DOCKER_CONFIG_FOLDER="/root/.docker"
+readonly DOCKER_CONFIG_FILE="$DOCKER_CONFIG_FOLDER/config.json"
+
+function remove_docker_config_file {
+  echo "Removing Docker config file $DOCKER_CONFIG_FILE"
+  sudo rm -f "$DOCKER_CONFIG_FILE"
+}
+
+function create_docker_config_file {
+  echo "Creating Docker config file $DOCKER_CONFIG_FILE"
+  trap remove_docker_config_file EXIT INT TERM
+  mkdir -p "$DOCKER_CONFIG_FOLDER"
+
+  # The DOCKER_REPO_XXX params are environment variables that ECS will set in the
+  # Jenkins Docker container. They are configured in the Jenkins Terraform
+  # templates.
+  cat << EOF > "$DOCKER_CONFIG_FILE"
+{
+  "auths": {
+    "$DOCKER_REPO_URL": {
+      "auth": "$DOCKER_REPO_AUTH",
+      "email": "$DOCKER_REPO_EMAIL"
+    }
+  }
+EOF
+}
+
+# The "sudo docker" command is necessary because Jenkins runs in a Docker
+# container, so it has to run other Docker containers as "siblings" using
 # a mounted Docker socket, and that socket is owned by the root user.
-./build-test-push.sh --docker-config-file "$DOCKER_REPO_URL" "$DOCKER_REPO_AUTH" "$DOCKER_REPO_EMAIL" --docker-command "sudo docker"
+./build-test-push.sh --docker-command "sudo docker"
--- build-test-push.sh
+++ build-test-push.sh
@@ -5,38 +5,7 @@
 
 set -e
 
-readonly DOCKER_CONFIG_FOLDER=~/.docker
-readonly DOCKER_CONFIG_FILE="$DOCKER_CONFIG_FOLDER/config.json"
 readonly DEFAULT_DOCKER_COMMAND="docker"
-
-function remove_docker_config_file {
-  echo "Removing Docker config file $DOCKER_CONFIG_FILE"
-  rm -f "$DOCKER_CONFIG_FILE"
-}
-
-function copy_docker_config_file {
-  if [ "$#" -ne 3 ]; then
-    echo "ERROR. You must pass exactly 3 arguments for a custom Docker config.json file: DOCKER_REPO_URL DOCKER_REPO_AUTH DOCKER_REPO_EMAIL."
-  fi
-
-  local readonly docker_repo_url="$1"
-  local readonly docker_repo_auth="$2"
-  local readonly docker_repo_email="$3"
-
-  echo "Creating Docker config file $DOCKER_CONFIG_FILE"
-  trap remove_docker_config_file EXIT INT TERM
-  mkdir -p "$DOCKER_CONFIG_FOLDER"
-
-  cat << EOF > "$DOCKER_CONFIG_FILE"
-{
-  "auths": {
-    "$docker_repo_url": {
-      "auth": "$docker_repo_auth",
-      "email": "$docker_repo_email"
-    }
-  }
-EOF
-}
 
 function build_docker_image {
   local readonly docker_cmd="$1"
@@ -69,13 +38,6 @@ function parse_command {
     local key="$1"
 
     case "$key" in
-      --docker-config-file)
-        local readonly docker_repo_url="$1"
-        local readonly docker_repo_auth="$2"
-        local readonly docker_repo_email="$3"
-        copy_docker_config_file "$docker_repo_url" "$docker_repo_auth" "$docker_repo_email"
-        shift 3
-        ;;
       --docker-command)
         docker_command="$2"
         shift
