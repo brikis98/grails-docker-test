1cf9052e45994976a652706ac6b0b1f62015df19
Yevgeniy Brikman <brikis98@gmail.com>
2015-12-10 14:14:41 -0500

    Add a simple run-prod script to workaround this container & AWS's weirdness with entrypoint

---
+++ run-prod.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Simple script that runs this Grails app in production mode. This is mostly
+# to work around the fact that the base Docker image for this Grails app didn't
+# set the environment up fully in its own Dockerfile, but instead initializes
+# parts of it using a script under //.gvm/bin/gvm-init.sh, which is executed
+# by each new command shell.
+
+set -e
+
+bash -l -c "grails prod $@ run-war"
